64f175f7bf08de3966bea0111c045fc6fff0b8ee
neeo <neeohe@outlook.com>
2025-12-06 16:45:04 +0800

    x

--- app.py
+++ app.py
@@ -456,15 +456,50 @@ def extract_domains():
     extract_domains()
 
 def generate_links(argo_domain: str):
-    try:
-        url = "https://api.ip.sb/geoip"
-        resp = requests.get(url, timeout=10)
-        print(f"meta: {resp.text}")
-        meta = resp.json()
-        ISP = f"{meta.get('asn_organization', 'Unknown')}-{meta.get('city', 'Unknown')}".replace(' ', '_')
-    except Exception as err:
-        print(f"Get ISP info error: {err}")
-        ISP = "Unknown"
+    def get_isp_from_ip_api():
+        try:
+            resp = requests.get(url, timeout=10)
+            print(f"ip-api meta: {resp.text}")
+            meta = resp.json()
+            org = meta.get('org', None)
+            city = meta.get('city', None)
+            ISP = f"{org}-{city}"
+        except Exception as err:
+            print(f"Get ISP info error: {err}")
+            ISP = None
+        return ISP
+
+    def get_isp_from_geoip():
+        try:
+            url = "https://api.ip.sb/geoip"
+            resp = requests.get(url, timeout=10)
+            print(f"geoio meta: {resp.text}")
+            meta = resp.json()
+            ISP = f"{meta.get('asn_organization', 'Unknown')}-{meta.get('city', 'Unknown')}".replace(' ', '_')
+        except Exception as err:
+            print(f"Get ISP info error: {err}")
+            ISP = None
+        return ISP
+
+    def get_isp_from_ipapi():
+        try:
+            url = "https://api.ipapi.is"
+            resp = requests.get(url, timeout=10)
+            print(f"ipapi meta: {resp.text}")
+            meta = resp.json()
+            org = meta.get('asn', {}).get('org', None)
+            city = meta.get('location', {}).get('city', None)
+            ISP = f"{org}-{city}"
+        except Exception as err:
+            print(f"Get ISP info error: {err}")
+            ISP = None
+        return ISP
+    
+    ISP = get_isp_from_ip_api()
+    if not ISP:
+        ISP = get_isp_from_geoip()
+    if not ISP:
+        ISP = get_isp_from_ipapi()
 
     VMESS = {
         "v": "2", "ps": f"{NAME}-{ISP}", "add": CFIP, "port": CFPORT, "id": UUID, "aid": "0",
